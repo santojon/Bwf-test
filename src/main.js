c78966fa1fa3132e669666f7595a06c77734ebab
Jonathan Santos <santojon5@gmail.com>
2016-07-10 16:30:35 +0000

    comment

--- src/main.js
+++ src/main.js
@@ -1,28 +1,31 @@
-// Mount front-end here
+/**
+ * Mount front-end here
+ */
+
 var currentUser = User.find({ username: 'santojon' });
 var allUsers = User.findAll().distinct();
 
-// add my name to screen
+// add current user username to screen
 var div = document.createElement('small');
 div.innerHTML = ' <code id="currentUser">' + currentUser.username + '</code>';
 document.getElementById('title').appendChild(div);
 
-// Add all defined dashboards for user 'me' to screen
+// Add all defined dashboards for user to screen
 var dashboards = Dashboard.findBy({ owner: currentUser }).distinct();
 dashboards.forEach(
-	function(v) {
-	    appendDashboard('dashboards', v);
+	function(dashboard) {
+	    appendDashboard('dashboards', dashboard);
 	}
 );
 
 allUsers.forEach(
-    function(v) {
-        if (v !== currentUser) {
+    function(user) {
+        if (user !== currentUser) {
             var li = document.createElement('li');
-            li.innerHTML = '<a id="' + v.username + '-li" ' + 'href="#">' + v.username + '</a>';
+            li.innerHTML = '<a id="' + user.username + '-li" ' + 'href="#">' + user.username + '</a>';
 		    document.getElementById('chg-lst').appendChild(li);
 
-		    document.getElementById(v.username + '-li').onclick = changeDashboard;
+		    document.getElementById(user.username + '-li').onclick = changeDashboard;
 		}
     }
 );
