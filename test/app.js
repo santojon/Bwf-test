14f7b4b3c6c7c3c0b0ce32320157f9a3bbb10ced
Jonathan santos <jgs@cin.ufpe.br>
2019-11-07 12:49:13 -0300

    tests fix

--- test/app.js
+++ test/app.js
@@ -1,27 +1,31 @@
 // Loads all needed things to run app. It need 'base.js' loaded to work
-loadScript('loader.js', function() {
-    // Timeout is needed to loader get all necessary data
-    setTimeout(function() {
+with(Base) {
+    loadScript('loader.js', function () {
+        // Timeout is needed to loader get all necessary data
+        setTimeout(function () {
 
-        /**
-         * The app itself.
-         */
+            /**
+             * The app itself.
+             */
 
-        var res = new Slpnr({ sets: specs }).run();
-        Object.keys(res).forEach(
-            function(tset) {
-                appendDiv('tests-results', tset, 'set');
-                appendSet(res[tset].relatedTo, res[tset]);
-                showProgress(res[tset]);
+            var res = new Slpnr({
+                sets: specs
+            }).run();
+            Object.keys(res).forEach(
+                function (tset) {
+                    appendDiv('tests-results', tset, 'set');
+                    appendSet(res[tset].relatedTo, res[tset]);
+                    showProgress(res[tset]);
 
-                res[tset].tests.forEach(
-                    function(result) {
-                        appendTest(tset, new Test(result));
-                    }
-                );
-            }
-        );
+                    res[tset].tests.forEach(
+                        function (result) {
+                            appendTest(tset, new Test(result));
+                        }
+                    );
+                }
+            );
 
-    // Adjust this time for your needs/requirements
-    }, 2500);
-});
+            // Adjust this time for your needs/requirements
+        }, 2500);
+    })
+};
